0dd251ba192b07754240208df5c5803851691ba4
asmaasheaba <asmaa.shiba2018@gmail.com>
2023-03-07 20:56:05 +0200

    ste

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -55,9 +55,9 @@
 
         @foreach ($user->friends as $friend)
        <div class="row col-4">
-        <img class="mb-5 col-6 frinimg" src="{{asset("$friend->image")}}" alt="friend">
-        <p class="col-4  col-6 pt-3">{{$friend->name}} </p>
-        <form  action="{{route('friends.destroy', $friend->id)}}" method="POST" class="col-3 py-2">
+        <img class="mb-5 col-5 frinimg" src="{{asset("$friend->image")}}" alt="friend">
+        <p class="col-4  col-5 pt-3">{{$friend->name}} </p>
+        <form  action="{{route('friends.destroy', $friend->id)}}" method="POST" class="col-2 py-2">
             @csrf
             @method('delete')
             <input  type='submit' class="btn btn-danger" value="unfriend" onclick="return confirm('Are you sure you want to delete this friend? ') ">
